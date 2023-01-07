45d84ee20290a5b0b45579fcb433760706758b3e
yeyulchoi <yeyulchoi@outlook.com>
2023-01-07 14:12:23 -0500

    created connection to mysql

---
+++ src/application/Main.java
@@ -0,0 +1,55 @@
+package application;
+import java.sql.*;
+import java.sql.SQLException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		
+		// This is a demo or connecting a java program to a database
+		//we will see how to open database connection.
+		
+		try
+		{
+			Connection con = null;
+			//Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/satishlesson","root","Yoonja7979!!");
+			System.out.println("Connection is successful.");
+			con.close();
+			
+			
+		}
+		catch(SQLException e)
+		{
+		System.out.println(e.getMessage());
+		}
+		
+		
+		
+		
+		
+		
+		/*
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("JavaDemoTwo.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		*/
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module JavaFxDemoTwo {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
